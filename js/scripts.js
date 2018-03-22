6cf3357890524e7128aa1e33316e738bda0c5a67
Brea Borlas and Luke Stockburger <brea@>
2018-03-22 13:41:43 -0700

    add example commit amend file

--- js/scripts.js
+++ js/scripts.js
@@ -4,15 +4,34 @@ $(document).ready(function() {
     var score = parseInt($("input:radio[name=coffee-type]:checked").val()) + parseInt($("input:radio[name=pet]:checked").val()) + parseInt($("#date-type").val()) +
     parseInt($("#person-type").val());
     if (score === 4) {
+      $("img").hide();
+      $(".mark").show();
+      $("#celebmatch").text("MARK RUFFALO, DAAAMN");
+      $("#match-header").show();
     }
-    else if () {
+    else if (score >= 5 && score <= 10) {
+      $("img").hide();
+      $(".emma").show();
+      $("#celebmatch").text("EMMA WATSON YOU LUCKY DOG");
+      $("#match-header").show();
     }
-    else if () {
+    else if (score >= 11 && score <= 15) {
+      $("img").hide();
+      $(".will").show();
+      $("#celebmatch").text(" THE FRESHEST GODDAMN PRINCE");
+      $("#match-header").show();
     }
-    else if () {
+    else if (score >= 16 && score <= 19) {
+      $("img").hide();
+      $(".gaga").show();
+      $("#celebmatch").text("LADY GAGA, OH MY GOD");
+      $("#match-header").show();
     }
     else {
-
+      $("img").hide();
+      $(".andy").show();
+      $("#celebmatch").text("unfortunately, you get DICK");
+      $("#match-header").show();
     }
   });
 });
