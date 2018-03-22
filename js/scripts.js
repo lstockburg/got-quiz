9b00e48368654c697f2d6e92980ecddf6d3d5656
Brea Borlas and Luke Stockburger <luke@>
2018-03-22 11:48:48 -0700

    get values from form on submit

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
 $(document).ready(function() {
-
+  $("#quiz").submit(function(event){
+    event.preventDefault();
+    var score = parseInt($("input:radio[name=coffee-type]:checked").val()) + parseInt($("input:radio[name=pet]:checked").val()) + parseInt($("#date-type").val()) +
+    parseInt($("#person-type").val());
+  });
 });
